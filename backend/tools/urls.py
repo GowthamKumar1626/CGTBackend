3d24d8c580b5e3a663a18991eefe00ec65c4721b
Gowtham Kumar <gowthamkumar20200@gmail.com>
2021-08-15 00:19:52 +0530

    Class based views using APIView implemented

--- backend/tools/urls.py
+++ backend/tools/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from tools.views import tool_list, tool_details
+# from tools.views import tool_list, tool_details
+from tools.views import ToolListAPIView, ToolDetailAPIView
 
+
+## APIView based Routes
 
 urlpatterns = [
-    path('list/', tool_list, name='tool-list'),
-    path('<int:pk>/', tool_details, name='tool-details')
+    path('list/', ToolListAPIView.as_view(), name='tool-list'),
+    path('<int:pk>/', ToolDetailAPIView.as_view(), name='tool-details')
 ]
+
+## Funtional APIs Routes
+
+# urlpatterns = [
+#     path('list/', tool_list, name='tool-list'),
+#     path('<int:pk>/', tool_details, name='tool-details')
+# ]
--- backend/tools/views.py
+++ backend/tools/views.py
@@ -1,39 +1,92 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework import status
 
 from tools.models import Tool
 from tools.serializers import ToolSerializer
 
-@api_view(['GET', 'POST'])
-def tool_list(request):
-    if request.method == 'GET':
+## Class based Views - APIView
+
+class ToolListAPIView(APIView):
+    
+    def get(self, request):
         tools = Tool.objects.all()
         serializer = ToolSerializer(tools, many=True)
         return Response(serializer.data)
-    if request.method == 'POST':
+        
+    def post(self, request):
         serializer = ToolSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+        
+
+class ToolDetailAPIView(APIView):
     
-@api_view(['GET', 'PUT', 'DELETE'])
-def tool_details(request, pk):
-    if request.method == 'GET':
-        tool = Tool.objects.get(pk=pk)
-        serializer = ToolSerializer(tool)
-        return Response(serializer.data)
-    if request.method == 'PUT':
-        tool = Tool.objects.get(pk=pk)
-        serializer = ToolSerializer(tool, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+    def get(self, request, pk):
+        try:
+            tool = Tool.objects.get(pk=pk)
+            serializer = ToolSerializer(tool)
             return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-    if request.method == 'DELETE':
-        tool = Tool.objects.get(pk=pk)
-        tool.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        except Tool.DoesNotExist:
+            return Response({"details": "Tool doesnot exit"}, status=status.HTTP_404_NOT_FOUND)
+    def put(self, request, pk):
+        try:
+            tool = Tool.objects.get(pk=pk)
+            serializer = ToolSerializer(tool, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors)
+        except Tool.DoesNotExist:
+            return Response({"details": "Tool doesnot exit"}, status=status.HTTP_404_NOT_FOUND)
+    def delete(self, request, pk):
+        try:
+            tool = Tool.objects.get(pk=pk)
+            tool.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Tool.DoesNotExist:
+            return Response({"details": "Tool doesnot exit"}, status=status.HTTP_404_NOT_FOUND)
+
+
+## Functional APIs
+
+# @api_view(['GET', 'POST'])
+# def tool_list(request):
+#     if request.method == 'GET':
+#         tools = Tool.objects.all()
+#         serializer = ToolSerializer(tools, many=True)
+#         return Response(serializer.data)
+#     if request.method == 'POST':
+#         serializer = ToolSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+    
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def tool_details(request, pk):
+#     if request.method == 'GET':
+#         try:
+#             tool = Tool.objects.get(pk=pk)
+#             serializer = ToolSerializer(tool)
+#             return Response(serializer.data)
+#         except Tool.DoesNotExist:
+#             return Response({"details": "Tool doesnot exit"}, status=status.HTTP_404_NOT_FOUND)
+#     if request.method == 'PUT':
+#         tool = Tool.objects.get(pk=pk)
+#         serializer = ToolSerializer(tool, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+#     if request.method == 'DELETE':
+#         tool = Tool.objects.get(pk=pk)
+#         tool.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
