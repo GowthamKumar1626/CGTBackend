02720e0baf49cb41979f849e76df0c37c52e3fd6
Gowtham Kumar <gowthamkumar20200@gmail.com>
2021-08-14 23:22:40 +0530

    Enable POST,PUT,DELETE Requests for tool view

--- backend/tools/serializers.py
+++ backend/tools/serializers.py
@@ -1,7 +1,18 @@
+from tools.models import Tool
 from rest_framework import serializers
 
 class ToolSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(read_only=True)
-    image = serializers.FileField(read_only=True)
-    description = serializers.CharField(read_only=True)
+    name = serializers.CharField()
+    image = serializers.FileField()
+    description = serializers.CharField()
+    
+    def create(self, validated_data):
+        return Tool.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.image = validated_data.get('image', instance.image)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        return instance
--- backend/tools/views.py
+++ backend/tools/views.py
@@ -1,18 +1,39 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 from tools.models import Tool
 from tools.serializers import ToolSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def tool_list(request):
-    tools = Tool.objects.all()
-    serializer = ToolSerializer(tools, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        tools = Tool.objects.all()
+        serializer = ToolSerializer(tools, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = ToolSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
     
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def tool_details(request, pk):
-    tool = Tool.objects.get(pk=pk)
-    serializer = ToolSerializer(tool)
-    return Response(serializer.data)
-    
+    if request.method == 'GET':
+        tool = Tool.objects.get(pk=pk)
+        serializer = ToolSerializer(tool)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        tool = Tool.objects.get(pk=pk)
+        serializer = ToolSerializer(tool, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    if request.method == 'DELETE':
+        tool = Tool.objects.get(pk=pk)
+        tool.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
